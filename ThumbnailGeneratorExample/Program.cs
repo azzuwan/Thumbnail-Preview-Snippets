85bcc1eb10171dc1ae05ef5967132f0d57c206b7
Azzuwan Aziz <azzuwan@gmail.com>
2015-07-24 03:20:11 +0800

    Inital commit

---
+++ ThumbnailGeneratorExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.WindowsAPICodePack.Shell;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace ThumbnailGeneratorExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            ShellFile shellFile = ShellFile.FromFilePath(args[0]);
+            Bitmap shellThumb = shellFile.Thumbnail.ExtraLargeBitmap;
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            shellThumb.Save(path + @"\Test", ImageFormat.Png);
+
+        }
+    }
+}
